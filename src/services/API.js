6d9357329e71d8bd7749f5943e0fde390f22c558
Ayana863 <ayana863anna@gmail.com>
2026-03-06 17:04:07 +0530

    url_add

--- src/services/API.js
+++ src/services/API.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://crm-backend-smaz.onrender.com"
 })
 
 // Add token automatically to headers
